2bb79844b567d989f52d053042cc264876d7a84b
behrad <wbdvlpr7@gmail.com>
2022-03-02 08:55:13 +0330

    03_category_route

--- course/03_category_route/task_03_category_route/lib/category_route.dart
+++ course/03_category_route/task_03_category_route/lib/category_route.dart
@@ -7,6 +7,9 @@
 import 'package:flutter/material.dart';
 
 // TODO: Check if we need to import anything
+import 'category.dart';
+
+final _backgroundColor = Colors.green[100];
 
 // TODO: Define any constants
 
@@ -47,16 +50,47 @@ class CategoryRoute extends StatelessWidget {
     // TODO: Create a list of the eight Categories, using the names and colors
     // from above. Use a placeholder icon, such as `Icons.cake` for each
     // Category. We'll add custom icons later.
+    final List<Category> categories = [];
+    for (var i = 0; i < _categoryNames.length; i++) {
+      categories.add(
+        Category(
+            name: _categoryNames[i],
+            color: _baseColors[i] as ColorSwatch<dynamic>,
+            iconLocation: Icons.cake),
+      );
+    }
 
     // TODO: Create a list view of the Categories
-    final listView = Container();
+    final listView = Container(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      color: _backgroundColor,
+      child: _buildListView(categories),
+    );
 
     // TODO: Create an App Bar
-    final appBar = AppBar();
+    final appBar = AppBar(
+      centerTitle: true,
+      elevation: 0.0,
+      backgroundColor: _backgroundColor,
+      title: const Text(
+        'Unit Converter',
+        style: TextStyle(
+          fontSize: 30.0,
+          color: Colors.black,
+        ),
+      ),
+    );
 
     return Scaffold(
       appBar: appBar,
       body: listView,
+    );
+  }
+
+  ListView _buildListView(List<Category> categories) {
+    return ListView.builder(
+      itemBuilder: (context, index) => categories[index],
+      itemCount: categories.length,
     );
   }
 }
--- course/03_category_route/task_03_category_route/lib/main.dart
+++ course/03_category_route/task_03_category_route/lib/main.dart
@@ -6,6 +6,7 @@
 import 'package:flutter/material.dart';
 
 // TODO: Import the CategoryRoute widget
+import 'category_route.dart';
 
 /// The function that is called when main.dart is run.
 void main() {
@@ -20,12 +21,12 @@ class UnitConverterApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Unit Converter',
       // TODO: Instead of pointing to exactly 1 Category widget,
       // our home should now point to an instance of the CategoryRoute widget.
-      home: Container(),
+      home: CategoryRoute(),
     );
   }
 }
